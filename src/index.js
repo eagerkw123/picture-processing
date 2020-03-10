bc193a7b180d9f297beec48865baff66d7166710
kewang015 <kw@winbaoxian.com>
2020-03-10 21:15:23 +0800

    修改引入方式

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const image2css = require('./image2css')
 const image2base = require('./image2base')
 
-module.exports = {
-	image2css,
-	image2base
-}
+exports.image2css = image2css
+exports.image2base = image2base
